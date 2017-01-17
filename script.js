ff8df336fb2affb198d484d0ebc2574bfd3825de
Денис Селетков < >
2017-01-17 23:27:04 +0400

    Started the Ball object

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var WIDTH = 777; // Board width, px
 var HEIGHT = 444; // Board height, px
 var SVG; // Board
 
+var RED = 0;
+var YELLOW = 1;
+var WHITE = 2;
+var BLACK = 3;
+
 // https://en.wikipedia.org/wiki/Blackball_(pool)
 
 document.addEventListener('DOMContentLoaded', init);
@@ -15,4 +20,42 @@ function init(event) {
 	SVG.addEventListener('contextmenu', function (event) {
 		event.preventDefault();
 	});
+}
+
+/*
+* Ball object.
+*
+* @param x Number X coord
+* @param y Number Y coord
+* @param color Number Ball color
+*
+* @returns Object Ball instance 
+*/
+function Ball(x, y, color) {
+	var v = {
+		x: 0,
+		y: 0
+	}
+	var p = {
+		x: x,
+		y: y
+	}
+	function move() {
+		p.x += v.x;
+		p.y += v.y;
+	}
+
+	return {
+		v: v,
+		move: move,
+		getPos: function() { 
+			return {
+				x: p.x, 
+				y: p.y
+			} 
+		},
+		getColor: function() {
+			return color
+		}
+	}
 }
