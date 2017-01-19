82f958a340be20e3804654cb19e15e9374467ae1
Денис Селетков < >
2017-01-19 22:44:14 +0400

    Added holes and added the table border

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 var WIDTH = 777; // Board width, px
 var HEIGHT = 444; // Board height, px
 var SVG; // Board
+var CIRCLE; // Circle SVG node (for various aims)
+var HOLE_RAD = 21; // Holes' radius, px
+var BALL_RAD = 19; // Ball radius, px
 
 var RED = 0;
 var YELLOW = 1;
@@ -20,6 +23,39 @@ function init(event) {
 	SVG.addEventListener('contextmenu', function (event) {
 		event.preventDefault();
 	});
+	CIRCLE = document.querySelector('#circle');
+	// create holes
+	CIRCLE.setAttribute("r", HOLE_RAD);
+	// 1
+	var hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", 0);
+	hole.setAttribute("cy", 0);
+	SVG.appendChild(hole);
+	// 2
+	hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", WIDTH / 2);
+	hole.setAttribute("cy", 0);
+	SVG.appendChild(hole);
+	// 3
+	hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", WIDTH);
+	hole.setAttribute("cy", 0);
+	SVG.appendChild(hole);
+	// 4
+	hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", WIDTH);
+	hole.setAttribute("cy", HEIGHT);
+	SVG.appendChild(hole);
+	// 5
+	hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", WIDTH / 2);
+	hole.setAttribute("cy", HEIGHT);
+	SVG.appendChild(hole);
+	// 6
+	hole = CIRCLE.cloneNode(); 
+	hole.setAttribute("cx", 0);
+	hole.setAttribute("cy", HEIGHT);
+	SVG.appendChild(hole);
 }
 
 /*
